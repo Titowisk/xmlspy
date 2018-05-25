2da3085835e870dea43fba496ef6cbeee3cedab0
Vitor Rabelo <rabelo51@gmail.com>
2018-05-25 15:50:31 -0300

    added xml_path_handler() and main() functions

--- xmlspy.py
+++ xmlspy.py
@@ -4,7 +4,7 @@
 # xmlspy reads all and copy those files into directories grouped by type an version
 
 #example:
-# ALESA_XMLS/XML 2013/ have xmls files CTE_1.04, NFE_2 and NFE_3.1
+# ALESA_XMLS/XML 2013/ have xmls files with types/versions CTE_1.04, NFE_2 and NFE_3.1
 # the result will create inside /ALESAT_XMLS/:
 #   /XML_2013_CTE_104/ containing only CTE_1.04 files;
 #   /XML_2013_NFE_200/ containing only NFE_2.00 files;
@@ -14,5 +14,57 @@ import argparse
 import xml.etree.ElementTree as ET
 import os
 
+"""
+Definition of parse
+
+The actual definition of "parse" in Wiktionary is "To split a file or other input into 
+pieces of data that can be easily stored or manipulated." 
+So we are splitting a string into parts then recognizing the parts to convert it into 
+something simpler than a string.
+"""
+
+# step 1
+# Receive an input of the directory that contains the xmls files
+# checks if there are xml files
+
+# step 2
+# iterate over the files to use the xml.etree.ElementTree module to parse
+# files according to its type (CTE_1.04, NFE_2 or NFE_3.1)
 
 
+# step 3
+# create a directory called <dir-name_type> for each type found and copy the files
+# with matching types inside these directories
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", help="path of the directory containing xmls files")
+    args = parser.parse_args()
+
+    xml_dir_path = args.path
+    xml_path_handler(xml_dir_path)
+
+    print(os.getcwd())
+
+# step 1
+
+def xml_path_handler(xml_dir_path):
+    # This function takes a path, check if there are xmls files, and return a iterator containing only xml files
+
+    # https://docs.python.org/3/library/os.html#os.scandir
+    # https://docs.python.org/3/library/os.html#os.DirEntry
+
+    xml_quantity = 0
+    dir_name = os.path.basename(xml_dir_path)
+    list_of_xml_files = []
+    for entry in os.scandir(xml_dir_path):
+        if entry.name.endswith('.xml'):
+            list_of_xml_files.append(entry)
+            xml_quantity += 1
+    if xml_quantity > 0:    
+        print("There are {0} xml files in {1}".format(xml_quantity, dir_name))
+    else:
+        print("There aren't xml files in this directory")
+    return list_of_xml_files
+
+main()
